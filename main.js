9ae2d7674aeb703f887d235d5cc50e1c95b3269c
loganlokku <sslogan1998@gmail.com>
2019-07-02 10:25:46 +0530

    first song

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 SC.initialize({
-  client_id: '2t9loNQH90kzJcsFCODdigxfp325aq4z'
+client_id:'95f22ed54a5c297b1c41f72d713623ef'
+
 });
 
-$(document).ready(function() {
-
-    SC.stream('/tracks/62899410',function(sound){
-      $('#start1').click(function(e) {
+$(document).ready(function()
+{ SC.stream('/tracks/632565285',function(sound){
+   $('#start').click(function(e) {
            e.preventDefault();
            sound.start();
          });
 
-      $('#stop1').click(function(e) {
-           e.preventDefault();
-           sound.stop();
-         });
-
-      });
-
+     $('#stop').click(function(e) {
+                    e.preventDefault();
+                    sound.stop();
+                  });
+});
  });
