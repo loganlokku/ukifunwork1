ed20013efbd7eb8bc1e749e1ba2e206294052290
loganlokku <sslogan1998@gmail.com>
2019-07-02 11:02:16 +0530

    first song

--- main.js
+++ main.js
@@ -5,16 +5,14 @@ client_id:'95f22ed54a5c297b1c41f72d713623ef'
 
 $(document).ready(function()
 { SC.stream('/tracks/632565285',function(sound){
-   $('#start').click(function(e) {
-           e.preventDefault();
-           sound.start();
-         });
+  $('#start').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
 
-     $('#stop').click(function(e) {
-                    e.preventDefault();
-                    sound.stop();
-                  });
+    $('#stop').click(function(e) {
+                   e.preventDefault();
+                   sound.stop();
+                 });
 });
- });
-
-/tracks/62899410
+});
