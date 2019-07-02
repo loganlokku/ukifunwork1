f50b59789d47ae7cb68e0ceec05cd7e39c31bcb7
loganlokku <sslogan1998@gmail.com>
2019-07-02 10:39:21 +0530

    second song

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ client_id:'95f22ed54a5c297b1c41f72d713623ef'
 });
 
 $(document).ready(function()
-{ SC.stream('/tracks/632565285',function(sound){
+{ SC.stream('/tracks/62899410',function(sound){
    $('#start').click(function(e) {
            e.preventDefault();
            sound.start();
@@ -14,5 +14,18 @@ $(document).ready(function()
                     e.preventDefault();
                     sound.stop();
                   });
+{ SC.stream('/tracks/62899410',function(sound){
+   $('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+     $('#stop1').click(function(e) {
+                    e.preventDefault();
+                    sound.stop();
+                  });
+
 });
  });
+
+
