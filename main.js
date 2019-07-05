26513aa38fccbeda3a1bee6dd64d316b397a5221
loganlokku <sslogan1998@gmail.com>
2019-07-05 14:12:58 +0530

    4 songs

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
+
+
 $(document).ready(function() {
   SC.stream('/tracks/62996783',function(sound){
 
@@ -11,6 +13,54 @@ $(document).ready(function() {
 
 
               $("#stop").click(function(e) {
+                 e.preventDefault();
+                 sound.stop();
+               });
+});
+});
+
+$(document).ready(function() {
+  SC.stream('/tracks/62996781',function(sound){
+
+       $("#start2").click(function(e) {
+              e.preventDefault();
+              sound.start();
+             });
+
+
+              $("#stop2").click(function(e) {
+                 e.preventDefault();
+                 sound.stop();
+               });
+});
+});
+
+$(document).ready(function() {
+  SC.stream('/tracks/62899723',function(sound){
+
+       $("#start3").click(function(e) {
+              e.preventDefault();
+              sound.start();
+             });
+
+
+              $("#stop3").click(function(e) {
+                 e.preventDefault();
+                 sound.stop();
+               });
+});
+});
+
+$(document).ready(function() {
+  SC.stream('/tracks/62896355',function(sound){
+
+       $("#start4").click(function(e) {
+              e.preventDefault();
+              sound.start();
+             });
+
+
+              $("#stop4").click(function(e) {
                  e.preventDefault();
                  sound.stop();
                });
